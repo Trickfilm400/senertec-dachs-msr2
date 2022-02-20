8f5838cf88024080fbeb4d9d93dd8442a0d40452
Nico Wagner <info@ni-wa.de>
2022-02-20 13:25:31 +0100

    Added main index class

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {ClientOptions} from "./interfaces/ClientOptions";
+import {HttpClient} from "./lib/httpClient";
+import ReadKeyList from "./lib/ReadKeyList";
+import keyTemplates from "./lib/keyTemplates";
+
+
+export class SenerTecDachsClientMSR2 {
+    private readonly options: ClientOptions;
+    private client: HttpClient;
+
+    constructor(options: ClientOptions) {
+        //double check if host is given
+        if (!options?.host) throw new Error("No Host given for SenerTec Dachs!");
+        //create http client
+        this.options = options;
+        this.client = new HttpClient(this.options);
+    }
+
+    fetchAll() {
+        let all = [
+            ...keyTemplates.BetriebsDatenDachs,
+            ...keyTemplates.Aktoren,
+            ...keyTemplates.BetriebsDaten3112,
+            ...keyTemplates.Temperatures,
+            ...keyTemplates.Daten2Waermeerzeuger,
+            ...keyTemplates.HydraulikSchema,
+            ...keyTemplates.MehrmodulTechnik,
+            ...keyTemplates.Tageslauf,
+            ...keyTemplates.Wartung
+        ];
+        return this.client.fetchByKeys(...all);
+    }
+
+    fetchUsefulData() {
+        let keys = [
+            ReadKeyList.Hka_Bd.ulArbeitElektr.key,
+            ReadKeyList.BD3112.Hka_Bd.ulArbeitElektr.key,
+            ReadKeyList.Hka_Bd.ulAnzahlStarts.key,
+            ReadKeyList.Hka_Mw1.usDrehzahl.key
+        ];
+        return this.client.fetchByKeys(...keys);
+    }
+}
